f44867c81a7fe8f90476e3905b7a8396c7e52c56
Joshua-SV <J.JSerna@hotmail.com>
2024-09-05 18:37:14 -0500

    Finished the scrollAnimation project

--- scrollAnimation/script.js
+++ scrollAnimation/script.js
@@ -1,1 +1,27 @@
 // Javascript
+
+// select all boxes
+const boxes = document.querySelectorAll('.box');
+
+
+// create event listener when user scrolls
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+    // we want our trigger height to be a little less than the original height because we want our boxes to show properly on screen
+    const triggerHeight = window.innerHeight / 5 * 4;
+
+    boxes.forEach( (box) => {
+        const topBox = box.getBoundingClientRect().top;
+
+        if (topBox < triggerHeight) {
+            box.classList.add('showBox');// add the class show box to boxes that fit the screen trigger
+        }
+        else {
+            box.classList.remove('showBox');
+        }
+    })
+}
+

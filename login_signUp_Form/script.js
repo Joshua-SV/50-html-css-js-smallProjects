3459693fc414aad35703702adc6810a7c91c5ec6
Joshua-SV <J.JSerna@hotmail.com>
2024-09-06 23:29:11 -0500

    Login style finished

--- login_signUp_Form/script.js
+++ login_signUp_Form/script.js
@@ -6,7 +6,7 @@ const labels = document.querySelectorAll('.form-control label');
 labels.forEach((label) => {
     let str = label.innerText.split('')
     .map((letter, indx) => {
-        return `<span style="transition-delay:${indx * 50}ms">${letter}</span>`
+        return `<span style="transition-delay:${indx * 70}ms">${letter}</span>`
     }).join('');
     // convert the text to array of letters then wrap these letters into spans then convert back to a string
 
